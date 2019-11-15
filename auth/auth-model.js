51236fa4f28ad109507fde084fdbc0b7a53b8c6d
Emmanuel Tanifum <etantanifum@gmail.com>
2019-11-15 12:27:16 -0500

    can register a new user

---
+++ auth/auth-model.js
@@ -0,0 +1,14 @@
+const db = require("../database/dbConfig.js");
+
+async function add(user) {
+    const [id] = await db("users").insert(user);
+    return findById(id);
+}
+
+function findById (id) {
+    return db("users").where({ id} ).first();
+}
+
+module.exports = {
+    add, findById
+}
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+
+const Users = require("./auth-model.js");
+
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(200).json(saved)
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Tough luck bro"})
+    })
 });
 
 router.post('/login', (req, res) => {
